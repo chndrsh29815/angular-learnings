736f73fc10a5172f2c4ba9040f1952e2600f6103
chandresh chandak <chandresh@purplefront.com>
2025-02-02 21:21:34 +0530

    lect-72 main file

--- angular-modules/src/main.ts
+++ angular-modules/src/main.ts
@@ -6,7 +6,10 @@ import {platformBrowserDynamic} from '@angular/platform-browser-dynamic'
 // //lect-67
 // import { AppModule } from './lect-67/app/app.module'
 
-//lect-71
-import { AppModule } from './lect-71/app/app.module'
+// //lect-71
+// import { AppModule } from './lect-71/app/app.module'
+
+//lect-72
+import { AppModule } from './lect-72/app/app.module'
 
 platformBrowserDynamic().bootstrapModule(AppModule)
