d454468801b8f7d0629585a4ce062e386bb73ac5
chandresh chandak <chandresh@purplefront.com>
2025-02-02 14:14:53 +0530

    lect-49

--- angular-essentials/src/main.ts
+++ angular-essentials/src/main.ts
@@ -65,7 +65,10 @@ import { bootstrapApplication } from '@angular/platform-browser';
 // //lect-46
 // import { AppComponent } from './lect-46/app/app.component';
 
-//lect-48
-import { AppComponent } from './lect-48/app/app.component';
+// //lect-48
+// import { AppComponent } from './lect-48/app/app.component';
+
+//lect-49
+import { AppComponent } from './lect-49/app/app.component';
 
 bootstrapApplication(AppComponent).catch((err) => console.error(err));
