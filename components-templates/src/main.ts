619f77a68e11af8adf1d4da80b8e7c1a7427cd80
chandresh chandak <chandresh@purplefront.com>
2025-02-03 15:13:21 +0530

    lect-102

--- components-templates/src/main.ts
+++ components-templates/src/main.ts
@@ -5,7 +5,10 @@ import { bootstrapApplication } from '@angular/platform-browser';
 // // lect-96
 // import { AppComponent } from './lect-96/app/app.component';
 
-// lect-98
-import { AppComponent } from './lect-98/app/app.component';
+// // lect-98
+// import { AppComponent } from './lect-98/app/app.component';
+
+// lect-102
+import { AppComponent } from './lect-102/app/app.component';
 
 bootstrapApplication(AppComponent).catch((err) => console.error(err));
