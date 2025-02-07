52f0d0f62c91b86bd02fceaf26cbe0d1b951e100
chandresh chandak <chandresh@purplefront.com>
2025-02-07 17:14:37 +0530

    lect-222

---
+++ http-requests/src/lect-222/app/places/available-places/available-places.component.ts
@@ -0,0 +1,36 @@
+import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
+
+import { Place } from '../place.model';
+import { PlacesComponent } from '../places.component';
+import { PlacesContainerComponent } from '../places-container/places-container.component';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs';
+
+@Component({
+  selector: 'app-available-places',
+  standalone: true,
+  templateUrl: './available-places.component.html',
+  styleUrl: './available-places.component.css',
+  imports: [PlacesComponent, PlacesContainerComponent],
+})
+export class AvailablePlacesComponent implements OnInit{
+  places = signal<Place[] | undefined>(undefined);
+  private httpClient = inject(HttpClient)
+  private destroyRef = inject(DestroyRef);
+
+  ngOnInit(): void {
+    const subscription = this.httpClient.get<{places: Place[]}>("http://localhost:3000/places").pipe(
+      map((resData) => {
+        return resData.places
+      })
+    ).subscribe({
+      next: (places) => {
+        this.places.set(places);
+      }
+    })
+
+    this.destroyRef.onDestroy(()=> {
+      subscription.unsubscribe()
+    })
+  }
+}
