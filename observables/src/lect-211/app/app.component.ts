2db2cacbe6f5ad8060f0374f857d1d58097c6160
chandresh chandak <chandresh@purplefront.com>
2025-02-07 14:11:20 +0530

    lect-211

---
+++ observables/src/lect-211/app/app.component.ts
@@ -0,0 +1,44 @@
+import { Component, DestroyRef, effect, inject, OnInit, signal } from '@angular/core';
+import {toObservable, toSignal} from '@angular/core/rxjs-interop'
+
+import { interval, map } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html',
+})
+export class AppComponent implements OnInit {
+  private destroyRef = inject(DestroyRef);
+  clickCount = signal(0);
+  clickCount$ = toObservable(this.clickCount);
+  interval$ = interval(1000);
+  intervalSignal = toSignal(this.interval$, {initialValue: 0});
+
+  constructor() {
+    // effect(() => {
+    //   console.log("clicked count : " + this.clickCount())
+    // })
+  }
+  ngOnInit(): void {
+    // const subscription = interval(1000).pipe(
+    //   map((val)=> val * 2)
+    // ).subscribe({
+    //   next: (val) => console.log(val),
+    // });
+    // this.destroyRef.onDestroy(() => {
+    //   subscription.unsubscribe();
+    // });
+    const subscription = this.clickCount$.subscribe({
+      next: val => console.log("clicked count : " + this.clickCount())
+    })
+
+      this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
+  }
+
+  onClick () {
+    this.clickCount.update(prevCount => prevCount + 1);
+  }
+}
