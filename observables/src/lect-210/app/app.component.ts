afffb39fc923c83d27829abae90877d555b08350
chandresh chandak <chandresh@purplefront.com>
2025-02-07 14:00:46 +0530

    lect-210

---
+++ observables/src/lect-210/app/app.component.ts
@@ -0,0 +1,42 @@
+import { Component, DestroyRef, effect, inject, OnInit, signal } from '@angular/core';
+import {toObservable} from '@angular/core/rxjs-interop'
+
+import { interval, map } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html',
+})
+export class AppComponent implements OnInit {
+  private destroyRef = inject(DestroyRef);
+  clickCount = signal(0);
+  clickCount$ = toObservable(this.clickCount);
+
+  constructor() {
+    // effect(() => {
+    //   console.log("clicked count : " + this.clickCount())
+    // })
+  }
+  ngOnInit(): void {
+    // const subscription = interval(1000).pipe(
+    //   map((val)=> val * 2)
+    // ).subscribe({
+    //   next: (val) => console.log(val),
+    // });
+    // this.destroyRef.onDestroy(() => {
+    //   subscription.unsubscribe();
+    // });
+    const subscription = this.clickCount$.subscribe({
+      next: val => console.log("clicked count : " + this.clickCount())
+    })
+
+      this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
+  }
+
+  onClick () {
+    this.clickCount.update(prevCount => prevCount + 1);
+  }
+}
