caa5ec02ccad9b9c4c9c4f09c8e90b0261bf0d2e
chandresh chandak <chandresh@purplefront.com>
2025-02-08 20:10:53 +0530

    lect-243

--- reactive-forms/src/main.ts
+++ reactive-forms/src/main.ts
@@ -2,7 +2,10 @@ import { bootstrapApplication } from '@angular/platform-browser';
 
 // import { AppComponent } from './app/app.component';
 
-// lect-239
-import { AppComponent } from './lect-239/app/app.component';
+// // lect-239
+// import { AppComponent } from './lect-239/app/app.component';
+
+// lect-243
+import { AppComponent } from './lect-243/app/app.component';
 
 bootstrapApplication(AppComponent).catch((err) => console.error(err));
