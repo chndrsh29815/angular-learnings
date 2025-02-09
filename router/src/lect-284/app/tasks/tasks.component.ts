ddd6b1e92c870b338cb82dc1c72d7ab6851a3a8b
chandresh chandak <chandresh@purplefront.com>
2025-02-09 17:55:51 +0530

    lect-285

--- router/src/lect-284/app/tasks/tasks.component.ts
+++ router/src/lect-284/app/tasks/tasks.component.ts
@@ -1,4 +1,12 @@
-import { Component, computed, DestroyRef, inject, input, OnInit } from '@angular/core';
+import {
+  Component,
+  computed,
+  DestroyRef,
+  inject,
+  input,
+  OnInit,
+  signal,
+} from '@angular/core';
 
 import { TaskComponent } from './task/task.component';
 import { Task } from './task/task.model';
@@ -15,20 +23,30 @@ import { ActivatedRoute, RouterLink } from '@angular/router';
 export class TasksComponent implements OnInit {
   userId = input.required<string>();
   //order = input<'asc' | 'desc'>();
-  order?: 'asc' | 'desc'; 
-  private tasksService = inject(TasksService)
+  order = signal<'asc' | 'desc'>('desc');
+  private tasksService = inject(TasksService);
 
-  userTasks = computed(()=> this.tasksService.allTasks().filter(task=> task.userId === this.userId()))
+  userTasks = computed(() =>
+    this.tasksService
+      .allTasks()
+      .filter((task) => task.userId === this.userId())
+      .sort((a, b) => {
+        if (this.order() === 'asc') {
+          return a.id > b.id ? -1 : 1;
+        } else {
+          return a.id > b.id ? 1 : -1;
+        }
+      })
+  );
 
   private activatedRoute = inject(ActivatedRoute);
-  private destroyRef = inject(DestroyRef)
+  private destroyRef = inject(DestroyRef);
 
   ngOnInit(): void {
     const subscription = this.activatedRoute.queryParams.subscribe({
-      next: params => this.order = params['order']
-    })
+      next: (params) => (this.order.set(params['order'])),
+    });
 
-    this.destroyRef.onDestroy(()=> subscription.unsubscribe());
+    this.destroyRef.onDestroy(() => subscription.unsubscribe());
   }
-
 }
