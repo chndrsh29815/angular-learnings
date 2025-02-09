df888885c94f3035c2a895b4bebae441089ae0bb
chandresh chandak <chandresh@purplefront.com>
2025-02-09 13:19:55 +0530

    lect-265

--- router/src/main.ts
+++ router/src/main.ts
@@ -2,8 +2,15 @@ import { bootstrapApplication } from '@angular/platform-browser';
 
 // import { AppComponent } from './app/app.component';
 
-// lect-263
-import { AppComponent } from './lect-263/app/app.component';
-import { appConfigs } from './lect-264/app/app.config';
+// // lect-263
+// import { AppComponent } from './lect-263/app/app.component';
+
+// // lect-264
+// import { AppComponent } from './lect-264/app/app.component';
+// import { appConfigs } from './lect-264/app/app.config';
+
+// lect-265
+import { AppComponent } from './lect-265/app/app.component';
+import { appConfigs } from './lect-265/app/app.config';
 
 bootstrapApplication(AppComponent, appConfigs).catch((err) => console.error(err));
