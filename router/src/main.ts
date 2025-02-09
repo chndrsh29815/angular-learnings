fbf5a4f7f668250abd96c91138ced6ac4465283b
chandresh chandak <chandresh@purplefront.com>
2025-02-09 16:39:59 +0530

    lect-281

--- router/src/main.ts
+++ router/src/main.ts
@@ -65,8 +65,12 @@ import { bootstrapApplication } from '@angular/platform-browser';
 // import { AppComponent } from './lect-279/app/app.component';
 // import { appConfigs } from './lect-279/app/app.config';
 
-// lect-280
-import { AppComponent } from './lect-280/app/app.component';
-import { appConfigs } from './lect-280/app/app.config';
+// // lect-280
+// import { AppComponent } from './lect-280/app/app.component';
+// import { appConfigs } from './lect-280/app/app.config';
+
+// lect-281
+import { AppComponent } from './lect-281/app/app.component';
+import { appConfigs } from './lect-281/app/app.config';
 
 bootstrapApplication(AppComponent, appConfigs).catch((err) => console.error(err));
